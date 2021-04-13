edd353036a9c861162dede315670722f60544fbd
Tuan <minhtuanle97@gmail.com>
2021-04-13 13:46:35 +0700

    Lop animal

---
+++ app/src/main/java/com/example/oop/Animal.java
@@ -0,0 +1,8 @@
+package com.example.oop;
+
+public class Animal {
+    // thuộc tính
+    String name;
+    String color;
+    // phương thức
+}
--- app/src/main/java/com/example/oop/MainActivity.java
+++ app/src/main/java/com/example/oop/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.oop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // object, class
+        // object : đại diện cho 1 thực thể
+        // class đại diện cho 1 quần thể
+        //class => kiểu dữ liệu
+        Animal meo = new Animal();
+        meo.color = "Xám";
+        meo.name = "meowmeow";
+        Log.d("BBB",meo.toString());
     }
 }
