394e3504c2ad91340a816eb1b4baaa46821aa558
Tuan <minhtuanle97@gmail.com>
2021-04-13 13:54:38 +0700

    phuong thuc khoi tao

--- app/src/main/java/com/example/oop/Animal.java
+++ app/src/main/java/com/example/oop/Animal.java
@@ -4,5 +4,9 @@ public class Animal {
     // thuộc tính
     String name;
     String color;
-    // phương thức
+    // phương thức khởi tạo
+    public Animal(String name, String color){
+        this.name = name;
+        this.color = color;
+    }
 }
--- app/src/main/java/com/example/oop/MainActivity.java
+++ app/src/main/java/com/example/oop/MainActivity.java
@@ -15,9 +15,7 @@ public class MainActivity extends AppCompatActivity {
         // object : đại diện cho 1 thực thể
         // class đại diện cho 1 quần thể
         //class => kiểu dữ liệu
-        Animal meo = new Animal();
-        meo.color = "Xám";
-        meo.name = "meowmeow";
-        Log.d("BBB",meo.toString());
+        Animal meo = new Animal("meowmeow","Xám");
+        Log.d("BBB",meo.name);
     }
 }
